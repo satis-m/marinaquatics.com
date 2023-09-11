09865f345908aee89681b6f55ce1e46e14fdf058
satish maharjan <satish.maharjan@apexcollege.edu.np>
2023-09-11 13:02:17 +0545

    fix: caseSensetive name

--- database/migrations/2023_07_20_105230_create_importers_table.php
+++ database/migrations/2023_07_20_105230_create_importers_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('Importers', function (Blueprint $table) {
+        Schema::create('importers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
         });
@@ -16,6 +16,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('Importers');
+        Schema::dropIfExists('importers');
     }
 };
