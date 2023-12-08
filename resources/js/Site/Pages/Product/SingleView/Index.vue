d88992fcb2c519e9ab516bfd87d61b6135525e13
satish maharjan <satish.maharjan@apexcollege.edu.np>
2023-12-08 14:13:42 +0545

    chore: resolve product image for bigger display

--- resources/js/Site/Pages/Product/SingleView/Index.vue
+++ resources/js/Site/Pages/Product/SingleView/Index.vue
@@ -53,10 +53,10 @@
                              :id="'preview-image-'+productInfo.main_picture?.id" role="tabpanel"
                              :aria-labelledby="'nav-image-'+productInfo.main_picture?.id"
                              tabindex="0">
-                          <ImageWithFallback v-if="mediaCheck('sm')" :source="productInfo.main_picture.thumbnail"
+                          <ImageWithFallback data-media="sm" v-if="mediaCheck('sm')" :source="productInfo.main_picture.thumbnail"
                                              :alt="productInfo.slug"/>
-                          <ImageWithFallback v-else :source="productInfo.main_picture.preview" :alt="productInfo.slug"/>
-
+                          <ImageWithFallback data-media="md"  v-else-if="mediaCheck('md')" :source="productInfo.main_picture.preview" :alt="productInfo.slug"/>
+                          <ImageWithFallback data-media="lg"  v-else :source="productInfo.main_picture.original" :alt="productInfo.slug"/>
                           <div class="tpproduct__info bage">
                                                         <span v-if="productInfo.current_discount != null"
                                                               class="tpproduct__info-discount bage__discount">{{
@@ -73,9 +73,10 @@
                              :id="'preview-image-'+alternativePicture?.id" role="tabpanel"
                              :aria-labelledby="'nav-image-'+alternativePicture?.id"
                              tabindex="0">
-                          <ImageWithFallback v-if="mediaCheck('sm')" :source="alternativePicture.thumbnail"
+                          <ImageWithFallback data-media="sm" v-if="mediaCheck('sm')" :source="alternativePicture.thumbnail"
                                              :alt="productInfo.slug"/>
-                          <ImageWithFallback v-else :source="alternativePicture.preview" :alt="productInfo.slug"/>
+                          <ImageWithFallback data-media="md" v-else-if="mediaCheck('md')" :source="alternativePicture.preview" :alt="productInfo.slug"/>
+                          <ImageWithFallback data-media="lg" v-else :source="alternativePicture.original" :alt="productInfo.slug"/>
                           <div class="tpproduct__info bage">
                                                         <span v-if="productInfo.current_discount != null"
                                                               class="tpproduct__info-discount bage__discount">{{
